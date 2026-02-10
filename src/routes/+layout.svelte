7e63b8d0c4c222b5d6fa1cf6b99157c4f7e12544
jasoncoawette <jason.coawette@gmail.com>
2026-02-09 17:07:51 -0700

    update: text styles h1, p. basic Layout. fix: 502

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <nav class="
 	sticky top-0
-	flex flex-row justify-center items-between
+	flex flex-row justify-start items-between
 	w-full max-w-4xl h-fit
 ">
 	<h1>Jason Coawette</h1>
@@ -16,6 +16,10 @@
 	<!--	Button	-->
 	<!--	Button  -->
 </nav>
-<main>
+<main class="
+	flex flex-col justify-start items-center
+	w-full max-w-4xl h-fit
+	mt-3
+">
 	{@render children()}
 </main>
